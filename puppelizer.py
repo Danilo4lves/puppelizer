830c9ee84fbfd93c187e78bbff726d1149f3208d
Danilo Alves <danilovieiraalves@hotmail.com>
2020-05-19 22:27:56 -0300

    refactor: Instead of getting .pp to .json file dump generated by conversion, it now creates .json file, loads it and removes it

--- puppelizer.py
+++ puppelizer.py
@@ -4,34 +4,46 @@ import os
 import glob
 import subprocess
 from BasicStructure import Initiate
-from CodeSmell import UnneccessaryAbstraction
+from CodeSmell import UnneccessaryAbstraction, ImperativeAbstraction
 
 PUPPET_FILES_PATH = '/Users/Alves/Desktop/puppetAnalysis'
-PUPPET_FILES_PATH_2 = '../puppetAnalysis'
-PUPPET_FILE_PATH = '/Users/Alves/Desktop/puppetAnalysis/filesinjson/define_with-body.json'
+PUPPET_FILES_PATH_2 = '../puppet-repositories'
+PUPPET_FILE_PATH = '/Users/Alves/Desktop/puppetAnalysis/filesInJson/alltypes.json'
 
 
-def runPuppelizerOnFile(file):
-    puppetParserCommand = '/opt/puppetlabs/bin/puppet parser dump --format json ' + file
-    subprocessObject = subprocess.run(
-        puppetParserCommand, shell=True, stdout=subprocess.PIPE)
-    subprocessOutput = str(subprocessObject.stdout)
-    puppetFileInJson = json.loads(subprocessOutput[2:-3])
+def runPuppelizerOnFile(file, isJsonFormatted=False):
+    if (not isJsonFormatted):
+        puppetParserCommand = '/opt/puppetlabs/bin/puppet parser dump --format json ' + \
+            file + ' > ' + file[:-3] + '.json'
+        subprocess.run(
+            puppetParserCommand, shell=True, stdout=subprocess.PIPE)
+
+        with open(file[:-3] + '.json') as jsonFile:
+            puppetFileInJson = json.load(jsonFile)
+
+        removeJsonFileCommand = 'rm ' + file[:-3] + '.json'
+        subprocess.run(removeJsonFileCommand, shell=True)
+    else:
+        with open(file) as jsonFile:
+            puppetFileInJson = json.load(jsonFile)
 
     obj = Initiate.pn_to_python(puppetFileInJson)
     ast = Initiate.construct(obj)
 
     UnneccessaryAbstraction.runner(ast)
+    # ImperativeAbstraction.runner(ast)
     print("\n-------------------\n")
 
 
 def runPuppelizerOnAllFiles(filesPath):
-    globPathName = filesPath + '/**/*.pp'
-    files = glob.glob(globPathName, recursive=True)
+    for file in os.listdir(filesPath):
+        globPathName = filesPath + '/' + file + '/**/*.pp'
+        print(globPathName)
+        files = glob.glob(globPathName, recursive=True)
 
-    for item in files:
-        runPuppelizerOnFile(item)
+        for item in files:
+            runPuppelizerOnFile(item)
 
 
 runPuppelizerOnAllFiles(PUPPET_FILES_PATH_2)
-# runPuppelizerOnFile(PUPPET_FILE_PATH)
+# runPuppelizerOnFile(PUPPET_FILE_PATH, True)
