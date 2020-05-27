9a470f7fa95e40c78785a16c0188b9a483fe7e64
Danilo Alves <danilovieiraalves@hotmail.com>
2020-05-27 16:24:29 -0300

    feat(puppelizer/BasicStructure/Constants): Implements constants' file

---
+++ BasicStructure/Constants.py
@@ -0,0 +1,5 @@
+PUPPET_REPOSITORIES_PATH = '../puppet-repositories'
+PUPPET_FILE_PATH = '../puppet-repositories/EslamElHusseiny--canvas-lms-dev-box/puppet/manifests/default.pp'
+RESULTS_FILE = "PuppelizerResult.csv"
+RESULTS_FILE_HEADER = "RepositoryName,PuppetFileCount,ClassCount,DefineCount,FileResourceCount,PackageResourceCount,\
+ServiceResourceCount,ExecCount,UnnecessaryAbs,ImperativeAbs,SyntaxErrorCount\n"
